1c9f41e3407cf0d64d93cb2fe3d48bbb5a249355
pupiecn <pupiecn@gmail.com>
2015-12-24 21:47:29 +0800

    add build scripts

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+git checkout 4530
+git stash
+git checkout master
+git pull origin
+git checkout 4530
+git merge master
+./scripts/feeds update -a
+./scripts/feeds install -a
+#read -n1 -p "Press any key to update custom packages..."
+
+#update git submodule packages
+git submodule init
+git submodule update
+#read -n1 -p "Press any key to continue..."
+git stash pop
+
+make defconfig
+
+echo "Make clean? Please answer yes or no."
+read YES_OR_NO
+case "$YES_OR_NO" in
+	yes|y|Yes|YES)
+		make clean
+		make V=99 -j 5
+	;;
+	
+	[nN]*)
+		echo "DO NOT make clean!"
+		make V=99 -j 5
+	;;
+
+	*)
+		echo "Sorry, $YES_OR_NO not recognized. Enter yes or no."
+		exit 1
+	;;
+esac
+
+
+
+
